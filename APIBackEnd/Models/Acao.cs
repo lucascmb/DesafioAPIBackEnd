39180b1b909327b17b978fb50288310e595b836a
lucascmb <lucas.cmb03@gmail.com>
2020-02-14 15:46:10 -0300

    + models

---
+++ APIBackEnd/Models/Acao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBackEnd.Models
+{
+    public class Acao
+    {
+        public int id { get; set; }
+        public enum tipoDaMovimentacao { Aplicação = 0, Resgate = 1 }
+        public int idDoFundo { get; set; }
+        public string cpfDoCliente { get; set; }
+        public decimal valorDaMovimentacao { get; set; }
+        public DateTime dataDaMovimentacao { get; set; }
+    }
+}
